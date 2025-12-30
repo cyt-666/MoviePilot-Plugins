85d517aeee09ec6bfe6b1e866d8ba74b20d0304e
chengyuntian <chengyt@damddos.com>
2025-12-30 17:34:17 +0800

    Rename plugin class to mediamsgwithdeletemsg

--- plugins.v2/mediamsgwithdeletemsg/__init__.py
+++ plugins.v2/mediamsgwithdeletemsg/__init__.py
@@ -17,7 +17,7 @@ from app.schemas.types import EventType, MediaType, MediaImageType, Notification
 from app.utils.web import WebUtils
 
 
-class mediaservermsgai(_PluginBase):
+class mediamsgwithdeletemsg(_PluginBase):
     """
     媒体服务器通知插件 AI增强版
 
