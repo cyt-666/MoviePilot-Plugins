2057f8e60cb4effd47978a15869b047deabbc146
Cheng Yuntian <yuntinachengbest@outlook.com>
2026-05-19 21:06:53 +0800

    fix: Bump MediaCoverGenerator version to 0.10.2 and remove pinned dependencies to avoid conflicts with MoviePilot

--- plugins.v2/mediacovergenerator/__init__.py
+++ plugins.v2/mediacovergenerator/__init__.py
@@ -55,7 +55,7 @@ class MediaCoverGenerator(_PluginBase):
     # 插件图标
     plugin_icon = "mediaplay.png"
     # 插件版本
-    plugin_version = "0.10.1"
+    plugin_version = "0.10.2"
     # 插件作者
     plugin_author = "cyt-666"
     # 作者主页
